1da99357a82c7ee63931fcd1ba7086ad1fc88763
lihaotong1997 <1772964666@qq.com>
2019-11-22 13:05:29 +0800

    htmusic

---
+++ htmusic/src/components/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<Head></Head>
+		<RotationChart></RotationChart>
+		<div>
+			<HomeContent></HomeContent>
+			<Sidebar></Sidebar>
+		</div>
+		<Foot></Foot>
+	</div>
+</template>
+
+<script>
+import Head from "../page/head.vue"
+import Foot from "../page/foot.vue"
+import HomeContent from "../page/homeContent.vue"
+import RotationChart from "../page/rotationChart.vue"
+import Sidebar from "../page/sidebar.vue"
+export default{
+	components:{Head,Foot,HomeContent,RotationChart,Sidebar}
+}
+</script>
+
+<style>
+</style>
---
+++ htmusic/src/page/rotationChart.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>轮播图</div>
+</template>
+
+<script>
+export default{
+	
+}
+</script>
+
+<style>
+</style>
